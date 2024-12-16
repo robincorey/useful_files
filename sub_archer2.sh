d9c04a9dd456c0e5036e74c5290fd6c06e05f3df
Robin Corey <robin.corey@gmail.com>
2024-12-16 08:43:22 +0000

    Create sub_archer2.sh

---
+++ sub_archer2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=job
+#SBATCH --nodes=4
+#SBATCH --tasks-per-node=128  # 4 x 32
+#SBATCH --cpus-per-task=1
+#SBATCH --time=48:00:00
+#SBATCH --array=1-3
+
+#SBATCH --account=e280-Stansfeld
+#SBATCH --partition=standard
+#SBATCH --qos=long
+
+# Setup the environment
+module load gromacs/2023.4 # gromacs/2024.2
+tpr=md_$SLURM_ARRAY_TASK_ID
+
+export OMP_NUM_THREADS=1
+
+srun --distribution=block:block --hint=nomultithread gmx_mpi mdrun -deffnm $tpr -s $tpr -cpi $tpr.cpt -nsteps 750000000
