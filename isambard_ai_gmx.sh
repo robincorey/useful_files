fad7d9787cf4b435ec54ba0dbec01d2380f2658a
Robin Corey <robin.corey@gmail.com>
2025-10-20 11:53:02 +0100

    Update isambard_ai_gmx.sh

--- isambard_ai_gmx.sh
+++ isambard_ai_gmx.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=md.out
 #SBATCH --time=24:00:00         # Hours:Mins:Secs
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=144     # there are 144 cpus per node
+#####SBATCH --cpus-per-task=144     # there are 144 cpus per node
 #SBATCH --array=1-5              # if you want repeats
 
 source /home/u5az/tr24321.u5az/gromacs-2024.4/build/scripts/GMXRC
